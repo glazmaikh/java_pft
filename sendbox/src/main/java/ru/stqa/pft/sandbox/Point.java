e5dbd51213fc8c91b2aecc28a5d0ac688b20c07f
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2023-04-13 11:19:01 +0300

    Добавлено несколько функций

---
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  private int x;
+  private int y;
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(p1.x);
+  }
+}

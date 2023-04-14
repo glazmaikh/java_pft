bfe601bab2018d4d27f1d34d9d756a10361d7c13
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2023-04-14 15:16:21 +0300

    Перенесена авторизация в @BeforeMethod

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -9,20 +9,21 @@ public class GroupCreationTests {
   private WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp() {
     System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver.exe");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-  }
 
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() {
     wd.findElement(By.linkText("groups")).click();
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("new")).click();
@@ -39,7 +40,7 @@ public class GroupCreationTests {
   }
 
   @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void tearDown() {
     wd.quit();
   }
 
